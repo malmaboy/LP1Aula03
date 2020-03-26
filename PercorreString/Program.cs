6a9e17249368921e0611a5aef08224c2fc95c579
Hugo Carvalho <huguinho66@live.com>
2020-03-26 10:00:47 +0000

    PercorreStings

---
+++ PercorreString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string yo, mee;
+            
+
+            Console.WriteLine("Insira uma frase: ");
+            yo = Console.ReadLine();
+            
+            Console.WriteLine("Insira um carater: ");
+            mee = Console.ReadLine();
+
+            foreach (char me in yo)
+            {
+                if (me != mee)
+                {
+                    Console.WriteLine(me);
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
